cc72034ceff11e655f81a633ea10c685120937cb
Sam Light <samlight1994@gmail.com>
2017-07-11 11:13:55 +0100

    Added support for different DBMS. Added postgres support.

--- database.py
+++ database.py
@@ -1,15 +1,32 @@
 import config as cfg
 import subprocess
 
-def dump():
-    fn = cfg.TMP_DIR + cfg.DB_FILENAME
-    command = [
+def __run(c): subprocess.call(' '.join(c), shell=True)
+
+def __dump_mysql(fn):
+    __run([
         'mysqldump',
         '-u', cfg.DB_USER,
         ('--password=%s' % cfg.DB_PASS),
         '--single-transaction',
         cfg.DB_NAME,
         '>', fn
-    ]
+    ])
 
-    subprocess.call(' '.join(command), shell=True)
+
+def __dump_postgres(fn):
+    __run([
+        'pg_dump',
+        ('--username=%s' % cfg.DB_USER),
+        ('--password=%s' % cfg.DB_PASS),
+        ('--dbname=%s' % cfg.DB_NAME)
+    ])
+
+def dump():
+    fn = cfg.TMP_DIR + cfg.DB_FILENAME
+    dbs = {
+        "mysql": __dump_mysql,
+        "postgres": __dump_postgres
+    }
+    f = dbs.get(cfg.DB_TYPE, lambda f: False)
+    return f(fn)
